c6c94ee0a3414a10b3ea6a6b81c3dc5a32238928
matthewgomezcullen <zcabgom@ucl.ac.uk>
2022-03-27 14:40:16 +0100

    Added working venv and requirements file

---
+++ AnnabelB/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!db9@w)6-v(1_)u4@%cnp)=#me8y40ch%&#4v3l+1j-np7o!%e'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ AnnabelB/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
