a88ba37d8f7d0ce3bda885cca2e621ad723f3c83
matthewgomezcullen <101350485+matthewgomezcullen@users.noreply.github.com>
2022-03-29 12:16:16 +0100

    Update base.py

--- AnnabelB/settings/base.py
+++ AnnabelB/settings/base.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
-    # 'whitenoise.runserver_nostatic'
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
